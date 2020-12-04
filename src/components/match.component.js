1af20ec2f53684ead253fdc65413f8fc98be5943
angelahuqing <angelahuqing@gmail.com>
2020-12-03 22:18:28 -0800

    Added cards to match page to showcase users

--- src/components/match.component.js
+++ src/components/match.component.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
+import  CardGroup from 'react-bootstrap/Card';
+
+import {Row, Col} from 'react-bootstrap';
+
 import {Container } from 'react-bootstrap';
 
 import Button from 'react-bootstrap/Button';
@@ -10,24 +14,65 @@ import { Link } from 'react-router-dom';
 //bc all we need to do is accept props and return jsx
 // export default class Match extends Component {
 
+    const styles = {
+        card: {
+          borderRadius: 55,
+          padding: '3rem',
+          marginTop: 100, 
+          marginBottom: 100, 
+          width: "50%"
+        },
+        cardImage: {
+          objectFit: 'cover',
+          borderRadius: 55
+        }
+      }
+
 const Match = () => {
     const cardInfo = [
-        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Angela", text: "ENFP"},
-        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Macy", text: "INFP"},
-        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Anna", text: "ENFJ"},
-        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Tanya", text: "ISTJ"},
-        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Lex", text: "ENFJ"},
+        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Angela", mb: "ENFP", comp:"5"},
+        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Macy", mb: "INFP", comp:"4"},
+        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Anna", mb: "ENFJ", comp:"4"},
+        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Tanya", mb: "ESFJ", comp:"3"},
+        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Lex", mb: "ENFJ", comp:"3"},
+        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Angela", mb: "ENFP", comp:"5"},
+        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Macy", mb: "INFP", comp:"4"},
+        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Anna", mb: "ENFJ", comp:"4"},
+        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Tanya", mb: "ESFJ", comp:"3"},
+        {image: "https://i.insider.com/50f967f56bb3f7830a000019", title:"Lex", mb: "ENFJ", comp:"3"}
     ];
 
     const renderCard = (card, index) => {
         return (
-                    <Card style={{ width: "25rem" }} key={index} className="text-center">
-                        <Card.Img variant="top" src="holder.js/100px180" src={card.image}/>
+            <Container fluid>
+                 {/* <CardGroup className="m-5 d-block"> */}
+                 <Row className="justify-content-md-center">
+                    <Card className="m-5 border-0 shadow" style={styles.card}>
+                    <Row>
+                        <Col>
+                        <Card.Img src="holder.js/100px180" src={card.image} className="rounded-circle"/>
+                        </Col>
+                        <Col>
                         <Card.Body>
-                        <Card.Title>{card.title}</Card.Title>
-                        <Card.Text>{card.text}</Card.Text>
+                        <Card.Title as="h1">{card.title}</Card.Title>
+                        <Card.Text as="h4">{card.mb}</Card.Text>
+                        <Card.Text as="h5">{card.comp}</Card.Text>
                         </Card.Body>
-                    </Card>               
+                        </Col>
+                    </Row>
+                    </Card>
+                </Row>
+                {/* </CardGroup> */}
+                {/* <Row className="justify-content-md-center">
+                        <Card style={styles.card} key={index} className={"text-center", "m-5 border-0 shadow", "m-5 d-block"}>
+                            <Card.Img variant="top" src="holder.js/100px180" src={card.image}/>
+                            <Card.Body>
+                            <Card.Title>{card.title}</Card.Title>
+                            <Card.Text>{card.text}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                </Row>   */}
+            </Container>            
         );
       };
     return <div>{cardInfo.map(renderCard)}</div>;
