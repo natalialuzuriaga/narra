801b1352b728c7efc3843b08fd85bed5618dea25
Roshni <rosh279@gmail.com>
2020-12-10 12:35:29 +0530

    installed axios; added array of matching personalities

--- src/components/match.component.js
+++ src/components/match.component.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MatchCard from "./matchCard.component";
 import FlatList from "flatlist-react";
 
+import axios from 'axios';
+
+const ENFP = "ENFP";
+const INFP = "INFP";
+const INFJ = "INFJ";
+const ENFJ = "ENFJ";
+const INTJ = "INTJ";
+const ENTJ = "ENTJ";
+const INTP = "INTP";
+const ENTP = "ENTP";
+const ISFP = "ISFP";
+const ESFP = "ESFP";
+const ISTP = "ISTP";
+const ESTP = "ESTP";
+const ISFJ = "ISFJ";
+const ESFJ = "ENFP";
+const ISTJ = "ENFP";
+const ESTJ = "ENFP";
+
 const Match = (props) => {
+  const [usersInfo, setUsersInfo] = useState([]);
+  //matches for personality types array
+  const matches = [
+    [INFJ, INTJ, INFP, ENFP, ENFJ, ENTJ, INTP, ENTP],
+    [INFJ, INTJ, INFP, ENFP, ENFJ, ENTJ, INTP, ENTP],
+    [INFJ, INTJ, INFP, ENFP, ENFJ, ENTJ, INTP, ENTP],
+    [INFJ, INTJ, INFP, ENFP, ENFJ, ENTJ, INTP, ENTP, ISFP],
+    [INFJ, INTJ, INFP, ENFP, ENFJ, ENTJ, INTP, ENTP],
+    [INFJ, INTJ, INFP, ENFP, ENFJ, ENTJ, INTP, ENTP],
+    [INFJ, INTJ, INFP, ENFP, ENFJ, ENTJ, INTP, ENTP, ESTJ],
+    [INFJ, INTJ, INFP, ENFP, ENFJ, ENTJ, INTP, ENTP],
+    [ENFJ, ESFJ, ESTJ],
+    [ISFJ, ISTJ],
+    [ESFJ, ESTJ],
+    [ISFJ, ISTJ],
+    [ESFP, ESTP, ISFJ, ESFJ, ISTJ, ESTJ],
+    [ISFP, ISTP, ISFJ, ESFJ, ISTJ, ESTJ],
+    [ESFP, ESTP, ISFJ, ESFJ, ISTJ, ESTJ],
+    [ISFP, ISTP, ISFJ, ESFJ, ISTJ, ESTJ, INTP],
+  ]
+  
   const cardInfo = [
     { image: "https://i.insider.com/50f967f56bb3f7830a000019", title: "Angela", mb: "ENFP", comp: "5" },
     { image: "https://i.insider.com/50f967f56bb3f7830a000019", title: "Macy", mb: "INFP", comp: "4" },
@@ -31,14 +71,18 @@ const Match = (props) => {
     );
   }
   return (
-      <ul>
-        <FlatList
-          list={cardInfo}
-          renderItem={renderCard}
-          renderWhenEmpty={() => <div>You have no suggested matches :(</div>}
-        />
-      </ul>
+    <ul>
+      <FlatList
+        list={cardInfo}
+        renderItem={renderCard}
+        renderWhenEmpty={() => <div>You have no suggested matches :(</div>}
+      />
+    </ul>
   )
 }
 
 export default Match;
+
+//get currently logged in user
+//store user.personalityType in a variable
+//switch statement
