6315d8a4b1820366976a443f151cdaf8e5b9c2c4
Raphael Gatchalian <raphaelg09@ucla.edu>
2020-12-10 02:34:49 -0800

    Resolved App.js conflict

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, withRouter, BrowserRouter} from "react-router-dom";
-
 import Nav from "./components/nav.component";
 import Login from "./components/login.component";
 import Register from "./components/register.component";
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <Router>
-      <LandingNav />
-      <br/>
-      <Route path="/login" exact component={Login} />
-      <Route path="/register" exact component={Register} />
-    </Router>
-  );
-}
-=======
 import Welcome from "./components/landing.component";
 import Match from "./components/match.component"
 
@@ -40,6 +26,5 @@ const App = () => (
   </BrowserRouter>
 )
 
->>>>>>> c97014a83ed408904d88d89c4d8958c588ce3d71
 
 export default App;
