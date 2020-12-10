db7829d4bfbb82dcc10f385ca320d01153dc3cd1
abhijaatgupta <abhijaat@gmail.com>
2020-12-10 01:23:46 -0800

    more of matching implementation

--- src/components/match.component.js
+++ src/components/match.component.js
@@ -24,7 +24,7 @@ const ESTJ = "5fd1c54299d8e0880b65106b";
 const Match = (props) => {
   const [usersInfo, setUsersInfo] = useState([]);
   const [dataFetched, setdataFetched] = useState(false);
-  const [currentUser, setcurrentUser] = useState([]);
+  const [currentUser, setcurrentUser] = useState();
   //matches for personality types array
   const matches = [
     [INFJ, INTJ, INFP, ENFP, ENFJ, ENTJ, INTP, ENTP],
@@ -45,11 +45,50 @@ const Match = (props) => {
     [ISFP, ISTP, ISFJ, ESFJ, ISTJ, ESTJ, INTP],
   ]
 
+  function getInd(personality) {
+    switch(personality){
+      case "ENFP":
+        return 0;
+      case "INFP":
+        return 1;
+      case "INFJ":
+        return 2;
+      case "ENFJ":
+        return 3;
+      case "INTJ":
+        return 4;
+      case "ENTJ":
+        return 5;
+      case "INTP":
+        return 6;
+      case "ENTP":
+        return 7;
+      case "ISFP":
+        return 8;
+      case "ESFP":
+        return 9;
+      case "ISTP":
+        return 10;
+      case "ESTP":
+        return 11;
+      case "ISFJ":
+        return 12;
+      case "ESFJ":
+        return 13;
+      case "ISTJ":
+        return 14;
+      case "ESTJ":
+        return 15;
+      default:
+        return -1;
+    }
+  }
+
   //get currently logged in user
   //store user.personalityType in a variable
   //switch statement for each personality type
     //set var index to number based on personality case
-  //declare array of potentialFriends
+  //declare array of potentialFriends(list of user IDs)
   //for length of array at matches[index]
     //get all people in db w that type and save in temp
     //potentialFriends = potentialFriends.concat(temp.users)
@@ -57,13 +96,52 @@ const Match = (props) => {
     //get entire user info and add to usersInfo
   //usersInfo has everything we need
 
+  const addToUsersInfo = (user) => {
+    let temp = usersInfo;
+    temp.push(user);
+    setUsersInfo(temp)
+  }
+
   useEffect(() => {
     if (!dataFetched){
       const curUserID = "5fcc72aa929bc4383602a196";
       axios.get('http://localhost:3000/users/'+curUserID)
-      .then(response => 
-        
-      )
+      .then(response => {
+        setcurrentUser(response);
+        const i = getInd(currentUser.personalityType);
+        var potentialFriends = [];
+        for (j = 0; j < matches[i].length; j++){
+          axios.get('http://localhost:3000/types/'+matches[i][j])
+          .then(response => {
+            potentialFriends = potentialFriends.concat(response.users);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+        }
+        for (j = 0; j < potentialFriends.length; j++){
+          axios.get('http://localhost:3000/users/'+potentialFriends[j])
+          .then(response => {
+            addToUsersInfo({
+               username: response.username,
+               personalityType: response.personalityType,
+               profilePicture: response.profilePicture,
+              //  password: response.body.password,
+               biography: response.biography,
+               //friends = req.body.friends;
+               snapchat: response.username,
+               instagram: response.instagram,
+              //  twitter: response.body.twitter,
+               facebook: response.username,
+               discord: response.discord,
+            })
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+        }
+        dataFetched = true;
+      })
       .catch((error) => {
         console.log(error);
       })
